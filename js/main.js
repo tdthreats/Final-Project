2db566939dc7ba09c9fd55be6d04edf436bc284a
tdthreats <tdthreats@Todds-MacBook-Pro-2.local>
2014-06-12 21:37:13 -0400

    Remove semicolon in script

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
         var map = new google.maps.Map(document.getElementById('map-canvas'),
           mapOptions);
 
-        setMarkers(map, musicSites);
+        setMarkers(map, musicSites)
       } 
 
 /**
